ae764327e387018b08c33f54f4b6862dad1336c3
Cixtor <cixtords@gmail.com>
2015-11-15 09:00:42 -0500

    Added implementation for the addition command

--- hostman.go
+++ hostman.go
@@ -5,12 +5,15 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"regexp"
 	"strings"
 )
 
 const config string = "/etc/hosts"
 
+var add = flag.String("add", "", "Add new entry to the hosts file")
 var export = flag.Bool("export", false, "List entries from the hosts file")
 
 type Hostman struct{}
@@ -47,6 +50,7 @@ func (obj *Hostman) Entries() Entries {
 		line = scanner.Text()
 
 		if line != "" {
+			entry = Entry{}
 			addresses = []string{}
 			line = strings.Replace(line, "\x20", "\t", -1)
 			sections = strings.Split(line, "\t")
@@ -76,6 +80,37 @@ func (obj *Hostman) Entries() Entries {
 	return entries
 }
 
+func (obj *Hostman) AddEntry(entry string) {
+	re := regexp.MustCompile(`^([0-9a-f:\.]{7,39})@(\S+)$`)
+	var parts []string = re.FindStringSubmatch(entry)
+
+	if len(parts) == 3 {
+		var formatted string
+		var address string = parts[1]
+		var domains string = parts[2]
+		file, err := os.OpenFile(config, os.O_APPEND|os.O_RDWR, 0644)
+
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			os.Exit(1)
+		}
+
+		defer file.Close()
+		domains = strings.Replace(domains, ",", "\x20", -1)
+		formatted = fmt.Sprintf("%s\t%s\n", address, domains)
+		_, err = io.WriteString(file, formatted)
+
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			os.Exit(1)
+		}
+
+		os.Exit(0)
+	} else {
+		fmt.Println("Invalid format in host entry")
+	}
+}
+
 func (obj *Hostman) ExportEntries() {
 	entries := obj.Entries()
 	result, err := json.MarshalIndent(entries, "", "\x20\x20")
@@ -103,7 +138,9 @@ func main() {
 
 	var manager Hostman
 
-	if *export == true {
+	if *add != "" {
+		manager.AddEntry(*add)
+	} else if *export == true {
 		manager.ExportEntries()
 	}
 
